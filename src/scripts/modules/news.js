0f4f4b1c317734523fcf6828044153b58a61b54d
vcw <byzov.iv@gmail.com>
2020-05-27 23:53:10 +0300

    Add first draft of a class for communication with news api

---
+++ src/scripts/modules/news.js
@@ -0,0 +1,30 @@
+export default class News {
+  constructor(apiKey) {
+    this.apiKey = apiKey;
+  }
+
+  _processResponseWithNews(response) {
+    let articles = response.articles;
+    articles.reduce((accumulator, current) => {
+      const { title, publishedAt, description, urlToImage } = current;
+      const source = current.source.name;
+      accumulator.push({
+        title,
+        publishedAt,
+        description,
+        urlToImage,
+        source
+      })
+      return accumulator;
+    }, []);
+    return articles;
+  }
+
+  getNews(keyword, fromDate) {
+    const url = `https://praktikum.tk/news/v2/everything?q=${keyword}&from=${fromDate}&pageSize=100&apiKey=3b4a1ba7f9c947e6a829644748b3bd90`;
+
+    return fetch(url)
+    .then(response => response.json())
+    .then(response => this._processResponseWithNews(response))
+  }
+}
